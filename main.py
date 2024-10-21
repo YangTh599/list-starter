9052abba07d9ed93c559fd7dfaad8ff9da0bb854
YangTh599 <YangTh599@northwested.org>
2024-10-21 10:38:30 -0400

    did work

---
+++ main.py
@@ -0,0 +1,26 @@
+# Thayer Yang
+# 21 OCT 2024
+# Lists
+
+import random
+from time import sleep
+
+# 3-1 Names
+
+names = ['Thayer','Apollos','Isaac','Josh','Lucas','Qwynn','Bryce','Charles','Koen','Elliot','Owen','Shay','Katelyn','Tucker','Nathan','Keily','Alex','Arden']
+
+print(names)
+sleep(1)
+# 3-2 Greetings
+
+for name in names:
+    print(f'Hello {name}!')
+    sleep(.5)
+
+# 3-3
+
+transportation_vechiles = ['Honda Odyssey', 'Toyota Sequoia','Toyota 4Runner', 'Honda Civic', 'Honda Sienna', 'VolksWagen Old School Van']
+
+for vechile in transportation_vechiles:
+    print(f'I would be pleased to own a {vechile} in the future.')
+    sleep(.5)
